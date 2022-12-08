0f015f404aabf8507ccf8e754d0e3a35d8207813
Elias <eliaseshetu2847@gmail.com>
2022-12-08 10:10:21 -0800

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - it prints an array of characters.
+ *Return: return 0
+ */
+int main(void)
+{
+	char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	putchar(str);
+	return (1);
+}
