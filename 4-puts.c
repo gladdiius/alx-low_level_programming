a0c59cf941ad567559e3718038ace05eabb7567d
Elias <eliaseshetu2847@gmail.com>
2022-12-08 06:28:15 -0800

    4. Hello, puts

--- 4-puts.c
+++ 4-puts.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-char str[50] = "\"Programming is like building a multilingual puzzle";
-puts(str);
-return (0);
+	char str[50] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
 }
