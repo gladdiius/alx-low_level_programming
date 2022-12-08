af139d395201d15b2d2a2e21151f8db846936729
Elias <eliaseshetu2847@gmail.com>
2022-12-08 06:31:13 -0800

    4. Hello, puts

--- 4-puts.c
+++ 4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main - it prints an array of characters
- *Return: return 0
- */
+*Return: return 0
+*/
 int main(void)
 {
 	char str[50] = "\"Programming is like building a multilingual puzzle";
