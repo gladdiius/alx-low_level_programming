8f3408f71e10ee910c78e1c2ddf5998c6179b9b1
Elias <eliaseshetu2847@gmail.com>
2022-12-08 06:03:36 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a function that print an array of char   
+ */
+int main(void) 
+{
+char str[] = "\"Programming is like building a multilingual puzzle"
+puts(str);
+return (0);
+}
---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - it prints an array of characters
+ *Return: return 0
+ */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
