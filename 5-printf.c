5b1ed4480f0cb5a0c7dfcb2b55008ec33d1e37cf
Elias <eliaseshetu2847@gmail.com>
2022-12-08 06:58:27 -0800

    5. Hello, printf

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - it prints an array of characters.
+ *Return: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
