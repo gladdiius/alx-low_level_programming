546cc7488426cc4f62d0f1ff0faca65c3d664387
Elias <eliaseshetu2847@gmail.com>
2022-12-09 06:56:01 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints number from 0 to 9
+ *
+ * Return: value 0
+ *
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
