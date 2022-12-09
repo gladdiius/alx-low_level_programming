169cf840820e4ab7a5078aee9c0714726ea939fe
Elias <eliaseshetu2847@gmail.com>
2022-12-09 13:19:35 -0800

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,17 @@
 int main(void)
 {
 	int j;
-	int i,k;
+	int i, k;
 	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	
+
 	for (k = 0; k <= 7; k++)
 	{
 	for (i = 0; i < 10; i++)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		if (array[j] != array[i] && array[j] - array[i] > 0 && i >= 1 && array[k] < array[i])
+		if (array[j] != array[i] && array[j] - array[i] > 0 &&
+			       	i >= 1 && array[k] < array[i])
 		{
 		putchar((array[k] % 10) + '0');
 		putchar((array[i] % 10) + '0');
