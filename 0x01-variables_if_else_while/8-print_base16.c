6094c4e56ed8ffe79332a3615ce470c4f3ff17c9
Elias <eliaseshetu2847@gmail.com>
2022-12-09 09:53:10 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char uppercase[]={'A','B','C','D','E','F'};
-	int  number[]={0,1,2,3,4,5,6,7,8,9};
+	char uppercase[]={'A', 'B', 'C', 'D', 'E', 'F'};
+	int  number[]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 	char lowercase;
 
