64e25ebf3dce63102bf8182ef34b4a7ebeba9704
Elias <eliaseshetu2847@gmail.com>
2022-12-09 10:10:35 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,15 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		putchar((i % 10) + '0');
 		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
