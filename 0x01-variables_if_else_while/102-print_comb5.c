c0d2fa9d747de13e73801bf9d9cd38a5c4994717
Elias <eliaseshetu2847@gmail.com>
2022-12-12 05:03:38 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,8 +10,7 @@
 
 int main(void)
 {
-	int j;
-	int i, k;
+	int j, i, k, l;
 	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	for (k = 0; k <= 9; k++)
 	{
@@ -21,7 +20,7 @@ int main(void)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		if (!(e[i] == 0 && e[j] == 0) && (e[l] != 9)
+		if (!(array[i] == 0 && array[j] == 0) && (array[l] != 9)
 		{
 		putchar(' ');
 		putchar((array[k] % 10) + '0');
