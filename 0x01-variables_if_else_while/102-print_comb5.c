9073f56f82f3fe09ef02da03cdab7ff4a5634c5d
Elias <eliaseshetu2847@gmail.com>
2022-12-12 04:59:35 -0800

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - prints all number form 0 to 9 separated by comma
+ *
+ * Return: value 0
+ *
+ **/
+
+int main(void)
+{
+	int j;
+	int i, k;
+	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	for (k = 0; k <= 9; k++)
+	{
+	for (l = 0; l <= 9; l++)
+	{
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+		if (!(e[i] == 0 && e[j] == 0) && (e[l] != 9)
+		{
+		putchar(' ');
+		putchar((array[k] % 10) + '0');
+		putchar((array[l] % 10) + '0');
+		putchar(' ');
+		putchar((array[i] % 10) + '0');
+		putchar((array[j] % 10) + '0');
+		putchar(',')
+		}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
