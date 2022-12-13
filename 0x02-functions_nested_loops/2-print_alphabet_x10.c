e141b8a9ce92572d3d3ed3e138e9b7138dee2c4a
Elias <eliaseshetu2847@gmail.com>
2022-12-13 06:50:19 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,14 @@
 
 void print_alphabet_x10(void)
 {
-	int j=9;
-	while(j <= 9)
+	int j;
+	for (j = 0; j <= 9; j++)
 	{
 		char n;
 		for(n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
-		j+=1;
 		_putchar('\n');
 	}
 }
