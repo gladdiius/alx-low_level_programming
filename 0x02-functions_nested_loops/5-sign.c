fd568ba0279f3fe74cfa08c5cef30c2e0577b266
Elias <eliaseshetu2847@gmail.com>
2022-12-13 08:48:32 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar((0%10)+'0');
+		_putchar((0 % 10) + '0');
 		return (0);
 	}
 	else
 	{
-	       _putchar('-');	
+	       _putchar('-');
 		return (-1);
 	}
 }
