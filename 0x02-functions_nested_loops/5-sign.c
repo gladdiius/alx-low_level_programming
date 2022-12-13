2c59758f8ab73a56aa5009a222b74a028b00d8a3
Elias <eliaseshetu2847@gmail.com>
2022-12-13 08:33:09 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_isalpha - check if a number is upper caser or lower
+ *@c: number passed from main
+ *Return: value 0 if not lowercase and 1 if uppercase
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if(n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
