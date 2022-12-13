0cff268e9fe5c3e485800f8cc3a1a5aa9923212b
Elias <eliaseshetu2847@gmail.com>
2022-12-13 11:56:25 -0800

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_last_digit - return table of numbers
+ *@x: number passed from main
+ *Return: value void
+ */
+void times_table(void)
+{
+	e[]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i, j;
+	for (j = 0; j <= 9; j++)
+	{
+	for (i = 0; i <= 9; i++)
+	{
+	table = e[j] * e[i];
+	_putchar((table % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+}
