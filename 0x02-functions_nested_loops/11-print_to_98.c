4ddce9bad6ab8b0edf205237bba11a51a48523a7
Elias <eliaseshetu2847@gmail.com>
2022-12-13 11:16:24 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *void print_to_98( - return x up to 98
+ *@x: number passed from main
+ *Return: value int
+ */
+void print_to_98(int n)
+{
+	int n;
+	while(n <= 98)
+	{
+		_putchar((x % 10) + '0');
+		if(x != 98)
+		{
+		_putchar(',');
+	}
+	}
+	return (0);
+}
