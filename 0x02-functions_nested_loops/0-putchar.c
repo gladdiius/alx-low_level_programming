d94a142d61f93f7b3e2b6443e0b88325cb04a0ae
Elias <eliaseshetu2847@gmail.com>
2022-12-13 03:42:25 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+/**
+ *
+ *main - calls _putchar to print str
+ *
+ *Return: value 0
+ *
+ */
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		_putchar(str[i]);
+	}
+	return 0;
+}
+
+/**
+ *
+ ** _putchar - writes the character c to stdout
+ *
+ ** @c: The character to print
+ *
+ **
+ *
+ ** Return: On success 1.
+ *
+ ** On error, -1 is returned, and errno is set appropriately.
+ *
+ **/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
