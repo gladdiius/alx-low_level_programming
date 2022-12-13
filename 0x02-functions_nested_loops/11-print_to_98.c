41db65b80f0da86b3f19872216773671144950d9
Elias <eliaseshetu2847@gmail.com>
2022-12-13 13:26:05 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 void print_to_98(int n)
 {
 	
-	while(n <= 98)
+	for(n; n <= 98; n++)
 	{
 		_putchar((n % 10) + '0');
 		if(n != 98)
@@ -16,6 +16,5 @@ void print_to_98(int n)
 		_putchar(',');
 		_putchar(' ');
 	}
-		n++;
 	}
 }
