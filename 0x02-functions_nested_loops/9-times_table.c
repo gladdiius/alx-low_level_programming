42da1aae142f82a8d9de4d65d4730099ebdadedc
Elias <eliaseshetu2847@gmail.com>
2022-12-14 10:54:58 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,6 +8,7 @@ void times_table(void)
 {
 	int e[]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i, j, table;
+
 	for (j = 0; j <= 9; j++)
 	{
 	for (i = 0; i <= 9; i++)
@@ -22,10 +23,16 @@ void times_table(void)
 		_putchar((table / 10) + '0');
 		_putchar((table % 10) + '0'); 
 	}
-	if (i != 9 && table < 10 && table * 2 <= 10)
+	if (i != 9 && table < 10 )
 	{
 		_putchar(',');
 		_putchar(' ');
+		_putchar(' ');
+	}
+	else if (i != 9 && table != 0 || table != 1 && table == j || j == 3 && table == 8 
+			   || j == 4 && table == 9 ||  j == 5 && table == 8 && table > 10)
+	{
+		_putchar(',');
 		_putchar(' ');
 	}
 	else
