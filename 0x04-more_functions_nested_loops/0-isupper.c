ea8365129b47832f3e960b2b1c71c5ad322c4337
Elias <eliaseshetu2847@gmail.com>
2022-12-15 05:24:18 -0800

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
-#include "main.h"
 /**
  * _isupper - check if a number is upppercase or lowercase
- * c: is an intiger
+ * @c: is an intiger
  * Return: return 1 if uppercase else 0
  */
 
 int _isupper(int c)
 {
-	int x;
-
-	x = isupper(c);
-	if (x == 2)
+	if (c > 'a' && x < 'z')
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
