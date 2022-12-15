f922b5bb1862a2c776f7fa6417031a95f1ef78d4
Elias <eliaseshetu2847@gmail.com>
2022-12-15 02:33:13 -0800

    3. Leap year

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,13 @@
+void print_remaining_days(int month, int day, int year)
+{
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
+}
