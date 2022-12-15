6490ef58916b518e0cdca8879c15927723affc52
Elias <eliaseshetu2847@gmail.com>
2022-12-15 11:37:52 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: is an intiger
+ * @b: is an intiger
+ * Return: return product of two numbers
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while(i < 9)
+	{
+	_putchar(i);
+	i++;
+	}
+	return;
+}
