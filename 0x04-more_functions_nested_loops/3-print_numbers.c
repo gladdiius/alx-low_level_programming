b59e97bcc59ffc2d866817c00a3ae5e1f965da69
Elias <eliaseshetu2847@gmail.com>
2022-12-16 08:01:53 -0800

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
 
 void print_numbers(void)
 {
-	int array[] ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	int array[] ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	while (i <= 9)
 	{
-		_putchar(i % 10 + '0');
+		_putchar(array[i] % 10 + '0');
 		i++;
 	}
 
