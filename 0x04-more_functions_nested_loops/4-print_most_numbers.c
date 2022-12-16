a230b14c1e13dd1e9a7150f6d5489a2ec0c6b63e
Elias <eliaseshetu2847@gmail.com>
2022-12-16 08:16:50 -0800

    header

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - print 1 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int array[] ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 || i != 5)
+		{
+		_putchar(array[i] % 10 + '0');
+		}
+		i++;
+	}
+
+	_putchar('\n');
+}
