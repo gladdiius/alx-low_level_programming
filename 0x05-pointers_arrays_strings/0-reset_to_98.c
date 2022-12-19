74dcae649ae0f7057131218dbacb56fc51e9fb67
Elias <eliaseshetu2847@gmail.com>
2022-12-19 09:36:42 -0800

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ *@n: argument 1
+ *Return: value void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this folder contains all the solutions for 0x05-pointers_arrays_strings project
