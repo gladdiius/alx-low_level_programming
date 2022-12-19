5f6da62507583c26645f94e5331657a8c620422a
Elias <eliaseshetu2847@gmail.com>
2022-12-19 13:40:10 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
- *print_rev - prints reversed string
+ *rev_string - prints reversed string
  *@s: argument 1
  *Return: void
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
-	strrev(s);
+	int x;
+
+	x = strlen(s);
+	while (x >= 0)
+	{
+		if (s[x] != '\0')
+		{
+			printf("%c", s[x]);
+		}
+		x -= 1;
+	}
 	putchar('\n');
 }
