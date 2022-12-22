17a7190d40435e39a8d4ba4c60ff733f946ed667
Elias <eliaseshetu2847@gmail.com>
2022-12-22 08:18:09 -0800

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatinat two strings
+ * @dest: string
+ * @src: string
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src));
+}
