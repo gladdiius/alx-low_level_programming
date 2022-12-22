16792dd03ba42a907360e6e0d75ea08f21c0cc4b
Elias <eliaseshetu2847@gmail.com>
2022-12-22 08:36:13 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strcmp.c - copy n chars from a sting
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: char
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
