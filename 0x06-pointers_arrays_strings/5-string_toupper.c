f7b12149ffaca1e1c0492de6f92e0ebb6f984e52
Elias <eliaseshetu2847@gmail.com>
2022-12-23 06:17:15 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ **string_toupper - convert string to uppercase
+ *@x: first argument
+ *Return: char
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+	while(x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+		{
+			x[i] = x[i] - 32;
+		}
+		i++;
+	}
+	return (x);
+}
