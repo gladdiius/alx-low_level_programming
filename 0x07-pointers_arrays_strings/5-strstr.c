06114622ba0935a503fa2ac54eb772549406fb3a
Elias <eliaseshetu2847@gmail.com>
2023-01-02 11:18:10 -0800

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - filters out a specied sub string from string
+ * @*haystack: string
+ * @*needle: string
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
