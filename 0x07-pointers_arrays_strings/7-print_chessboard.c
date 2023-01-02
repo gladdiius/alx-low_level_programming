17300644b3f3698bc5aed59d0eb6f14d325fb6fa
Elias <eliaseshetu2847@gmail.com>
2023-01-02 11:46:50 -0800

    5

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: 2d array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8]);
+{
+	int i;
+	char j;
+	for(i = 0; i < 8; i++)
+	{
+		j = a[1][i];
+		_putchar(j);	
+	}
+	putchar('\n');
+	for(i = 0; i < 8; i++)
+	{
+		j = a[7][i];
+		_putchar(j);
+	}
+}
