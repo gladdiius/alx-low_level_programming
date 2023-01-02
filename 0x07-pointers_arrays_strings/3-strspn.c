e5a220778acfa89d03e44ae725042ce2f1b9989a
Elias <eliaseshetu2847@gmail.com>
2023-01-02 10:40:56 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strchr - check if a string is in set of specified segment
+ * @s: string
+ * @accept: string
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
