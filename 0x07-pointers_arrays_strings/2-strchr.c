5e8079b08782b5122061839a91f82c628ad31383
Elias <eliaseshetu2847@gmail.com>
2023-01-02 10:25:57 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strchr - check the occurance of a char
+ * @s: string
+ * @c: char
+ * Return: null if char doesn't occur or char if it ocuur
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
