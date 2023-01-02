5995f1dc4756fb8db17ae6c129add897674d222b
Elias <eliaseshetu2847@gmail.com>
2023-01-02 11:02:52 -0800

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strspn - filters out a specied sub string from string
+ * @s: string
+ * @accept: string
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
