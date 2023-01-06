e1127f5f08e2282f85fafd7f3735993487a501cf
Elias <eliaseshetu2847@gmail.com>
2023-01-06 13:42:13 -0800

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	int x = atoi(argv[1]) * atoi(argv[2]);
 
-	printf("%d", x);
+	printf("%d\n", x);
 	return (0);
 }
