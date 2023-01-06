56381bbb7551afa517d1d99be268eabbd2e0e6f4
Elias <eliaseshetu2847@gmail.com>
2023-01-06 12:59:33 -0800

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print number of args
+ *@argc:int
+ *@argv:char
+ *Return: value 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argc);
+	return (0);
+}
