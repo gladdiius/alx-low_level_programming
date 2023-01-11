06d404215b94e701d3248ff0e724158bf992b06c
Elias <eliaseshetu2847@gmail.com>
2023-01-11 10:03:28 -0800

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+/**
+ *create_array -creats an arrray
+ *@size: unsigned int
+ *@c: character
+ *Return: char
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	int i = 0;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	ar = malloc(size * sizeof(c));
+	while (i < size)
+	{
+
+		if (i == size--)
+
+		{
+			ar[i] = '\0';
+		}
+		ar[i] = c;
+		i++;
+	}
+	return ar;
+
+}
