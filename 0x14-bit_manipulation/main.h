8f4278df18ad1458e275060e538cee9db2c72926
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-03 17:11:21 +0300

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+#include <unistd.h>
+#include <unistd.h>
+int _putchar(char c);
 #include <ctype.h>
 #include <stddef.h>
 #endif
