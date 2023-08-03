49f5b7dfe6a30e414f4cd6956e0ae18e2e4e4256
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-03 17:12:04 +0300

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_binary - prints a binary value of a given number
+ *@n: number to be converted to binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 15, mask, set = 0;
+
+	while (i >= 0)
+	{
+		mask = 1 << i;
+		if (n & mask)
+		{
+			_putchar('1');
+			set = 1;
+		}
+		else if (set)
+			_putchar('0');
+		i--;
+	}
+	if (n == 0)
+		_putchar('0');
+}
