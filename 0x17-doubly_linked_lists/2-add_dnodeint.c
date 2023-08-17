68c53f4ab0539208172007cd40c2c55dd517d53f
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-17 16:22:15 +0300

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,8 +21,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	h = *head;
 
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
 
 	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
 	*head = new;
 
 	return (new);
