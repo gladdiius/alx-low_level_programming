b435fb2c2568275645f525a95cff598015e52f10
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-08 20:34:00 +0300

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -29,6 +29,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #include <stdlib.h>
-
 #endif
