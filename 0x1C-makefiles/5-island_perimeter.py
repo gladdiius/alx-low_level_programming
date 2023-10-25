2c60cced898f079250537390b0e7857120d4b967
elias_eshetu <eliaseshetu28@gmail.com>
2023-10-25 15:25:18 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python3
+
 def island_perimeter(grid):
-    """returns the perimeter of island"""
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (list): A list of lists representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Raises:
+        ValueError: If the grid is not rectangular or exceeds the maximum width and height.
+
+    """
+
+    if not all(len(row) == len(grid[0]) for row in grid) or len(grid) > 100 or len(grid[0]) > 100:
+        raise ValueError("Invalid grid dimensions")
+
     perimeter = 0
-    for first_scan in grid:
-        for second_scan in first_scan:
-            if second_scan == 1:
-                perimeter += 2
-    return perimeter + 2
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
