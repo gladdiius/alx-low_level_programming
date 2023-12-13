630d5c665d87a26abe6277ba726149cc16fdaa11
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-13 21:32:46 +0300

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,55 +1,48 @@
 #include "search_algos.h"
 /**
- * print_array - this function prints an array
- * @start: the start of the array
- * @end: the end of the array
- * @array: the array
- */
-void print_array(int *array, int start, int end)
-{
-	int i;
-
-	for (i = start; i <= end; i++)
-		if (i != end)
-			printf("%d, ", array[i]);
-		else
-			printf("%d", array[i]);
-	printf("\n");
-}
-/**
  * binary_search - search algorithm
  * @array: the array to be searched
  * @size: the size of the array
  * @value: the value to be searched
  * Return: the value if found or -1 if not found
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	int low = 0, high = size - 1;
-	int mid, mid_element;
+	int left = 0;
+	int right = size - 1;
+	int mid, i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
-	printf("Searching in array: ");
-	print_array(array, low, high);
-	while (low <= high)
+
+	while (left <= right)
 	{
-		mid = low + (high - low) / 2;
-		mid_element = array[mid];
+		mid = (left + right) / 2;
+
 		printf("Searching in array: ");
-		print_array(array, mid, high);
-		if (mid_element == value)
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
 		{
 			return (mid);
 		}
-		else if (mid_element < value)
+		else if (array[mid] < value)
 		{
-			low = mid + 1;
+			left = mid + 1;
 		}
 		else
 		{
-			high = mid - 1;
+			right = mid - 1;
 		}
 	}
+
 	return (-1);
 }
+
